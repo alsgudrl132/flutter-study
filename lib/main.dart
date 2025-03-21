bcce9fdf83842b9f10e6189e05fda3f9d43d6595
alsgudrl132 <alsgudrl132@gmail.com>
2025-03-21 13:41:18 +0900

    route의 활용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_study/screen/main_screen.dart';
+import 'package:flutter_study/screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,39 +14,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MainScreen2(),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => SplashScreen(),
+        '/main' : (context) => MainScreen(),
+      },
     );
   }
 }
 
-// Widget이란 앱의 모든 구성요소, 화면에 그려지는 모든것을 위젯으로 표현가능하다.
-// StatelessWidget (상태가 없는 위젯) - 한 번 생성되면 내부데이터나 상태를 변경할 수 없고, UI를 그리기위한 정보만을 가짐
-
-class MainScreen2 extends StatefulWidget {
-  const MainScreen2({super.key});
-
-  @override
-  State<MainScreen2> createState() => _MainScreen2State();
-}
-
-class _MainScreen2State extends State<MainScreen2> {
-  String msg = "안녕하세요";
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration(seconds: 3),() {
-      setState(() {
-        msg = "안녕하세요2";
-      });
-    },);
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("나의 첫 앱"),), body: Text(msg),);
-  }
-}
 
